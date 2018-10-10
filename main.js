ad04a66efced4f98ca6480e4efb54dc97b76bb0a
axis06 <withcomputer@gmail.com>
2018-10-10 13:10:44 +0900

    [fix] send device motin setting

--- main.js
+++ main.js
@@ -120,29 +120,12 @@ $(function(){
 
 
   window.addEventListener("devicemotion", function(evt){
-    var xg = evt.accelerationIncludingGravity.x; // 横方向の傾斜
-    var yg = evt.accelerationIncludingGravity.y; // 縦方向の傾斜
-    var zg = evt.accelerationIncludingGravity.z; // 上下方向の傾斜
-    var angle={};
-    angle.y = Math.floor(Math.atan2(yg,zg)/Math.PI * 180);
-    angle.x = Math.floor(Math.atan2(xg,zg)/Math.PI * 180);
-    angle.z = Math.floor(Math.atan2(yg,xg)/Math.PI * 180);
-    if(angle.x < 0){
-      angle.x += 360;
-    }
-    if(angle.y < 0){
-      angle.y += 360;
-    }
-    if(angle.z < 0){
-      angle.z += 360;
-    }
 
-    
     self_connect.send({
       "time": Date.now(),
-      "x": angle.x,
-      "y": angle.y,
-      "z": angle.z}
+      "x": evt.accelerationIncludingGravity.x,
+      "y": evt.accelerationIncludingGravity.y,
+      "z": evt.accelerationIncludingGravity.z}
     );
 
   }, true);
