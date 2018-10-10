40b016a798d2e58bfc49de88eb32d09198356877
axis06 <withcomputer@gmail.com>
2018-10-10 10:05:46 +0900

    [add] menu

---
+++ main.js
@@ -0,0 +1,13 @@
+$(function(){
+$("#Headset").click(function() {
+  $("#menu").hide();
+  $("#headset_side").show();
+});
+
+$("#Computational").click(function() {
+  $("#menu").hide();
+  $("#clomputatuional_side").show();
+});
+
+
+}); 
