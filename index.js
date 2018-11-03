9dbabe69c2dcd8c63270fe1ddfda7b6b601a32e1
axis06 <withcomputer@gmail.com>
2018-11-04 08:57:29 +0900

    [fix]

--- index.js
+++ index.js
@@ -79,7 +79,7 @@ $(function(){
     const videoSource = $('#videoSource').val();
     const constraints = {
       audio: {deviceId: audioSource ? {exact: audioSource} : true},
-      video: true,
+      video: undefined,
     }
 
     
