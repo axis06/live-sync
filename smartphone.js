59832836786b025e291cc9d6eaa501a82ca96bb6
axis06 <withcomputer@gmail.com>
2019-01-22 18:43:22 +0900

    [fix] sm js

--- smartphone.js
+++ smartphone.js
@@ -1,58 +1,41 @@
-
-
 $(function(){
   let localStream;
-  var connecting_flag = false;
-  var self_pos = "headset";
-  var dataConnection; 
-  var self_connect;
-  var time = 0;
-  var x,y,z = 0;
 
-  $("#Headset").click(function() {
-    $("#menu").hide();
-    $("#headset_side").show();
+  let existingCall;
+  var self_connect
+
+  const peer = new Peer({
+    key:   "64584427-b066-4ec8-89d4-02db55ae61a3",
+    debug: 0,
   });
 
-  $("#Computational").click(function() {
-    $("#menu").hide();
-    $("#clomputatuional_side").show();
-    self_pos = "computational"
-
-    var sock = new WebSocket('ws://127.0.0.1:3100');
-
-    sock.addEventListener('open',function(e){
-        console.log('Socket Connecting');
-    });
-
-
-    peer.listAllPeers(peers => {
-      $.each( peers, function( key, value ) {
-        if(peer.id != value){
-          const call = peer.call(value,localStream);
-          self_connect = peer.connect(value)
-          self_connect.on('data', data => get_command(data));
-
-          call.on('stream', stream => {
-            const el = $('#video').get(0);
-            el.srcObject = stream;
-            el.play();
-
-            connecting_flag = true;
-            view_connecting()
-          });
-      
-          call.on('close', () => {
-            console.log('connection closed');
-          });
-        }
-      });
-    });
+  peer.on('call', call => {
+    call.answer(localStream);
+    connection_data(call);
   });
+
+  peer.on('error', err => {
+    console.error(err.message);
+  });
+
+  peer.on('open', () => { 
+    console.log(peer.id)
+    source_select()
+  });
+
+  peer.on('connection', c => {
+    c.on('open', () => openEv(c));
+  });
+
+  function openEv(c){
+    self_connect = c
+   
+    c.on('data', data => {console.log(data)})
+  }
 
   const audioSelect = $('#audioSource');
   const videoSelect = $('#videoSource');
-  const selectors = [audioSelect, videoSelect];
+  const selectors = [audioSelect,videoSelect];
 
   navigator.mediaDevices.enumerateDevices()
     .then(deviceInfos => {
@@ -87,159 +70,301 @@ $(function(){
         }
       });
 
-      videoSelect.on('change', setup);
-      audioSelect.on('change', setup);
+      audioSelect.on('change', source_select);
     });
-    setup();
 
-  const peer = new Peer({
-    key:   "64584427-b066-4ec8-89d4-02db55ae61a3",
-    debug: 3,
-  });
 
-  peer.on('open', () => {
-    view_connecting()
-  });
 
-  peer.on('connection', c => {
-    self_connect = c;
-    c.on('open')
-    c.on('data', data => setup());
-  });
-
-  function setup() {
+  function source_select() {
+    // Get audio/video stream
     const audioSource = $('#audioSource').val();
-    const videoSource = $('#videoSource').val();
     const constraints = {
-      audio: {deviceId: audioSource ? {exact: audioSource} : true},
-      video: {deviceId: videoSource ? {exact: videoSource} : true},
-    };
+      audio: true,
+      video: false,
+    }
+
 
     navigator.mediaDevices.getUserMedia(constraints).then(stream => {
-      $('#local').get(0).srcObject = stream;
-      localStream = stream;
+
+      const $canvas = document.createElement('canvas');
+      $canvas.width = $canvas.height = 1;
+      $canvas.getContext('2d');
+      const vStream = $canvas.captureStream();
+    
+      const [vTrack] = vStream.getVideoTracks();
+      const [aTrack] = stream.getAudioTracks();
+    
+      localStream =  new MediaStream([vTrack, aTrack]);
+
+      if (existingCall) {
+        existingCall.replaceStream(localStream);
+        return;
+      }
+      
+      auto_connect();
+
     }).catch(err => {
+      $('#step1-error').show();
       console.error(err);
     });
   }
 
-  peer.on('call', call => {
-    call.answer(localStream);
+  function connection_data(call){
+    if (existingCall) {
+      existingCall.close();
+    }
     call.on('stream', stream => {
-      const el = $('#video').get(0);
+      const el = $('#their-video').get(0);
       el.srcObject = stream;
       el.play();
-
-
-      connecting_flag = true
-      view_connecting()
     });
 
-  });
+    existingCall = call;
 
-  peer.on('error', err => {
-    alert(err.message);
-    connecting_flag = false;
-    view_connecting()
-  });
-
-  function view_connecting(){
-    var status = "no connect"
-    if(connecting_flag == true){status = "connected";
-    $("#connect-info-get").hide()
-  
-    }
-
-    $('.connect-info').html(
-      "CONNECTING<br>" +
-      "[PeerID] " + peer.id + "<br>" +
-      "[Status] " + status + "<br>" +
-      "[Ping] " + time  + "ms<br>"
-      );
   }
 
 
-  function view_data(){
-    if(connecting_flag){
-    $('.data-info').html(
-      "CONNECTING<br>" +
-      "[x] " + x + "<br>" +
-      "[y] " + y + "<br>" +
-      "[z] " + z + "<br>"
-      );
-    }
-  }
+  function auto_connect(){
+    peer.listAllPeers(peers => {
+      $.each( peers, function( key, value ) {
+        if(peer.id != value){
 
-  function get_command(data){
-    time = Date.now() - data.time;
-    x = data.x;
-    y = data.y;
-    z = data.z;
+          console.log("c:"+value)
+          const call = peer.call(value, localStream);
+          self_connect = peer.connect(value)
+          self_connect.on('data', data => get_command(data));
+          connection_data(call)
 
-
-    var json_parse = JSON.stringify({"osc":"WsOscSend","path":"/wek/inputs","type":"fff","data":[data.x,data.y,data.z]});
-
-
-    sock.send(json_parse);
-
-    view_connecting();
-    view_data();
+          call.on('close', () => {
+            console.log('connection closed');
+          });
+        }
+      });
+    });
   }
 
 
-  window.addEventListener("devicemotion", function(evt){
-    console.log(evt);
-    self_connect.send({
-      "time": Date.now(),
-      "x": evt.accelerationIncludingGravity.x,
-      "y": evt.accelerationIncludingGravity.y,
-      "z": evt.accelerationIncludingGravity.z});
-    
-  }, true);
-
-
-}); 
-
-
-AFRAME.registerComponent('hide-once-playing', {
-  schema: {type: 'selector'},
-  init: function () {
-    this.onPlaying = this.onPlaying.bind(this);
-    this.onPause = this.onPause.bind(this);
-  },
-  play: function () {
-    if (this.data) {
-      this.data.addEventListener('playing', this.onPlaying);
-      this.data.addEventListener('pause', this.onPause);
+ $(".a-enter-vr-button").click(function() {
+  if (!document.fullscreenElement) {
+    document.documentElement.requestFullscreen();
+  } else {
+    if (document.exitFullscreen) {
+      document.exitFullscreen(); 
     }
-  },
-  pause: function () {
-    if (this.data) {
-      this.data.removeEventListener('playing', this.onPlaying);
-      this.data.removeEventListener('pause', this.onPause);
-    }
-  },
-  onPlaying: function (evt) {
-    this.el.setAttribute('visible', false);
-  },
-  onPause: function (evt) {
-    this.el.setAttribute('visible', true);
   }
 });
 
-AFRAME.registerComponent('play-on-window-click', {
-  init: function () {
-    this.onClick = this.onClick.bind(this);
-  },
-  play: function () {
-    window.addEventListener('click', this.onClick);
-  },
-  pause: function () {
-    window.removeEventListener('click', this.onClick);
-  },
-  onClick: function (evt) {
-    var video = this.el.components.material.material.map.image;
-    if (!video) { return; }
-    video.play();
+  var count = 0;
+
+  AFRAME.registerComponent('camera-devise', {
+    init: function () {
+      this.currentPosition= new THREE.Quaternion();
+    },
+    tick: function () {
+      count += 1;
+      if(count >= 10){
+        count = 0;
+        var currentPosition = this.currentPosition;
+        this.el.object3D.updateMatrixWorld();
+        currentPosition = this.el.object3D.quaternion;
+
+        if(self_connect != null){
+          self_connect.send({
+            "time": Date.now(),
+            "x": currentPosition.x,
+            "y": currentPosition.y,
+            "z": currentPosition.z,
+            "w": currentPosition.w
+          });
+        }
+      }
+
+    },
+  
+  });
+});
+$(function(){
+  let localStream;
+
+  let existingCall;
+  var self_connect
+
+  const peer = new Peer({
+    key:   "64584427-b066-4ec8-89d4-02db55ae61a3",
+    debug: 0,
+  });
+
+  peer.on('call', call => {
+    call.answer(localStream);
+    connection_data(call);
+  });
+
+  peer.on('error', err => {
+    console.error(err.message);
+  });
+
+  peer.on('open', () => { 
+    console.log(peer.id)
+    source_select()
+  });
+
+  peer.on('connection', c => {
+    c.on('open', () => openEv(c));
+  });
+
+  function openEv(c){
+    self_connect = c
+   
+    c.on('data', data => {console.log(data)})
   }
+
+  const audioSelect = $('#audioSource');
+  const videoSelect = $('#videoSource');
+  const selectors = [audioSelect,videoSelect];
+
+  navigator.mediaDevices.enumerateDevices()
+    .then(deviceInfos => {
+      const values = selectors.map(select => select.val() || '');
+      selectors.forEach(select => {
+        const children = select.children(':first');
+        while (children.length) {
+          select.remove(children);
+        }
+      });
+
+      for (let i = 0; i !== deviceInfos.length; ++i) {
+        const deviceInfo = deviceInfos[i];
+        const option = $('<option>').val(deviceInfo.deviceId);
+
+        if (deviceInfo.kind === 'audioinput') {
+          option.text(deviceInfo.label ||
+            'Microphone ' + (audioSelect.children().length + 1));
+          audioSelect.append(option);
+        } else if (deviceInfo.kind === 'videoinput') {
+          option.text(deviceInfo.label ||
+            'Camera ' + (videoSelect.children().length + 1));
+          videoSelect.append(option);
+        }
+      }
+
+      selectors.forEach((select, selectorIndex) => {
+        if (Array.prototype.slice.call(select.children()).some(n => {
+          return n.value === values[selectorIndex];
+        })) {
+          select.val(values[selectorIndex]);
+        }
+      });
+
+      audioSelect.on('change', source_select);
+    });
+
+
+
+  function source_select() {
+    // Get audio/video stream
+    const audioSource = $('#audioSource').val();
+    const constraints = {
+      audio: true,
+      video: true,
+    }
+
+
+    navigator.mediaDevices.getUserMedia(constraints).then(stream => {
+
+      const $canvas = document.createElement('canvas');
+      $canvas.width = $canvas.height = 1;
+      $canvas.getContext('2d');
+      const vStream = $canvas.captureStream();
+    
+      const [vTrack] = vStream.getVideoTracks();
+      const [aTrack] = stream.getAudioTracks();
+    
+      localStream =  new MediaStream([vTrack, aTrack]);
+
+      if (existingCall) {
+        existingCall.replaceStream(localStream);
+        return;
+      }
+      
+      auto_connect();
+
+    }).catch(err => {
+      $('#step1-error').show();
+      console.error(err);
+    });
+  }
+
+  function connection_data(call){
+    if (existingCall) {
+      existingCall.close();
+    }
+    call.on('stream', stream => {
+      const el = $('#their-video').get(0);
+      el.srcObject = stream;
+      el.play();
+    });
+
+    existingCall = call;
+
+  }
+
+
+  function auto_connect(){
+    peer.listAllPeers(peers => {
+      $.each( peers, function( key, value ) {
+        if(peer.id != value){
+
+          console.log("c:"+value)
+          const call = peer.call(value, localStream);
+          self_connect = peer.connect(value)
+          self_connect.on('data', data => get_command(data));
+          connection_data(call)
+
+          call.on('close', () => {
+            console.log('connection closed');
+          });
+        }
+      });
+    });
+  }
+
+
+ $(".a-enter-vr-button").click(function() {
+  if (!document.fullscreenElement) {
+    document.documentElement.requestFullscreen();
+  } else {
+    if (document.exitFullscreen) {
+      document.exitFullscreen(); 
+    }
+  }
+});
+
+  var count = 0;
+
+  AFRAME.registerComponent('camera-devise', {
+    init: function () {
+      this.currentPosition= new THREE.Quaternion();
+    },
+    tick: function () {
+      count += 1;
+      if(count >= 10){
+        count = 0;
+        var currentPosition = this.currentPosition;
+        this.el.object3D.updateMatrixWorld();
+        currentPosition = this.el.object3D.quaternion;
+
+        if(self_connect != null){
+          self_connect.send({
+            "time": Date.now(),
+            "x": currentPosition.x,
+            "y": currentPosition.y,
+            "z": currentPosition.z,
+            "w": currentPosition.w
+          });
+        }
+      }
+
+    },
+  
+  });
 });
